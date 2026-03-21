8eb7459cd4925154e63fd804da14fb263048ce63
THEWOLF2004 <ugominichino@icloud.com>
2026-03-21 18:06:54 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+const AFK_CHANNEL_ID = process.env.AFK_CHANNEL_ID;
+const LOG_CHANNEL_ID = process.env.LOG_CHANNEL_ID;
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+  if (newState.channelId === AFK_CHANNEL_ID && oldState.channelId !== AFK_CHANNEL_ID) {
+    const logChannel = newState.guild.channels.cache.get(LOG_CHANNEL_ID);
+    if (logChannel) {
+      logChannel.send(`☣ ${newState.member.user.username} is now AFK`);
+    }
+  }
+});
+
+client.login(process.env.TOKEN);
